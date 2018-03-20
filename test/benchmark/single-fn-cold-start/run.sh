b2c7a0542766486e3c83a84ba134cf88b3e195ba
Ta-Ching Chen <contact@tachingchen.com>
2018-03-20 16:41:32 +0800

    update test script

--- test/benchmark/single-fn-cold-start/run.sh
+++ test/benchmark/single-fn-cold-start/run.sh
@@ -14,21 +14,28 @@ echo "Creating nodejs env"
 fission env create --name nodejs --image fission/node-env --poolsize 10
 trap "fission env delete --name nodejs" EXIT
 
-echo "Creating function"
-fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js
-trap "fission fn delete --name $fn" EXIT
-
-echo "Creating route"
-fission route create --function $fn --url /$fn --method GET
-
-echo "Waiting for router to catch up"
 sleep 10
 
-echo "Benchmarking for single cold-start time"
-# -e is not support in k6 official release yet.
-# k6 run -e FN_ENDPOINT="http://$FISSION_ROUTER/$fn" sample.js
-export FN_ENDPOINT="http://$FISSION_ROUTER/$fn"
-k6 run sample.js
+for i in {0..10}
+do
+    echo "Creating function"
+    fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js
+
+    echo "Creating route"
+    fission route create --function $fn --url /$fn --method GET
+
+    echo "Waiting for router to catch up"
+    sleep 5
+
+    echo "Benchmarking for single cold-start time"
+    # -e is not support in k6 official release yet.
+    # k6 run -e FN_ENDPOINT="http://$FISSION_ROUTER/$fn" sample.js
+    export FN_ENDPOINT="http://$FISSION_ROUTER/$fn"
+    k6 run sample.js
+    # ab -n 1 -c 1 $FN_ENDPOINT
+
+    fission fn delete --name $fn
+done
 
 # crappy cleanup, improve this later
 kubectl get httptrigger -o name | tail -1 | cut -f2 -d'/' | xargs kubectl delete httptrigger
