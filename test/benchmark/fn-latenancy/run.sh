4ec6449f16056e238c88bf2edcc34892adad9103
Ta-Ching Chen <contact@tachingchen.com>
2018-03-20 19:27:03 +0800

    add function latency test

--- test/benchmark/fn-latenancy/run.sh
+++ test/benchmark/fn-latenancy/run.sh
@@ -14,32 +14,34 @@ trap "fission env delete --name nodejs" EXIT
 
 sleep 10
 
+fn=nodejs-hello-$(date +%s)
+
+echo "Creating function"
+fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js
+
+echo "Creating route"
+fission route create --function $fn --url /$fn --method GET
+
+echo "Waiting for router to catch up"
+sleep 5
+
+echo "Benchmarking for single cold-start time"
+
 for i in {0..10}
 do
-    fn=nodejs-hello-$(date +%s)
-
-    echo "Creating function"
-    fission fn create --name $fn --env nodejs --code $ROOT/examples/nodejs/hello.js
-
-    echo "Creating route"
-    fission route create --function $fn --url /$fn --method GET
-
-    echo "Waiting for router to catch up"
-    sleep 5
-
-    echo "Benchmarking for single cold-start time"
     # -e is not support in k6 official release yet.
     # k6 run -e FN_ENDPOINT="http://$FISSION_ROUTER/$fn" sample.js
     export FN_ENDPOINT="http://$FISSION_ROUTER/$fn"
 
     # extract average request time from output
-    k6 run sample.js | grep "http_req_duration" | awk '{print $2}'| sed 's/.*avg=*\(.*\).*/\1/' >> time.txt
+    k6 run --duration 60 --vus 100 sample.js | grep "http_req_duration" | awk '{print $2}'| sed 's/.*avg=*\(.*\).*/\1/' >> time.txt
     # ab -n 1 -c 1 $FN_ENDPOINT
-
-    fission fn delete --name $fn
 done
+
+fission fn delete --name $fn
 
 # crappy cleanup, improve this later
 kubectl get httptrigger -o name | tail -1 | cut -f2 -d'/' | xargs kubectl delete httptrigger
 
 echo "All done."
+#!/usr/bin/env bash
